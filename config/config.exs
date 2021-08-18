b89a3eb132e49b61c7cb71a164d27751c08506a1
Vincent Vella <vincevella0@gmail.com>
2021-08-18 01:36:31 -0400

    feat: add chron job

---
+++ config/config.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+config :pluralsight_tweet, PluralsightTweet.Scheduler, jobs: [
+  {"* * * *", fn ->
+    PluralsightTweet.FileReader.get_strings_to_tweet(
+      Path.join("#{:code.priv_dir(:pluralsight_tweet)}", "sample.txt")
+    )
+    |> PluralsightTweet.TweetServer.tweet
+  end
+  }
+]
